9b5cedf3b698cf87acb2228ee941f4c72d4aee1b
Thomas Sobor <sobor.thomas@yahoo.com>
2020-10-25 17:24:58 -0700

    app testing

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,15 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "../App";
+import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it("shows a comment box", () => {
-  const div = document.createElement("div");
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-  ReactDOM.render(<App />, div);
-
-  // Looks inside the div
-  // and checks to see if the CommentBox is in there
-  expect(div.innerHTML).toContain("Comment Box");
-
-  ReactDOM.unmountComponentAtNode(div);
+it("shows a comment list", () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
